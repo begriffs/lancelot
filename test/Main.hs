fab5e295128c34d7df88a44df89a6c98b012b569
Joe Nelson <cred+github@begriffs.com>
2016-12-31 18:29:53 -0800

    More b64 props

--- test/Main.hs
+++ test/Main.hs
@@ -1,7 +1,11 @@
 module Main where
 
 import qualified TestBase64 as TB64
+import System.Exit
 
-main :: IO Bool
-main =
-  TB64.runTests
+main :: IO ()
+main = do
+  good <- and <$> sequence [TB64.runTests]
+  if good
+     then exitSuccess
+     else exitFailure
--- test/TestBase64.hs
+++ test/TestBase64.hs
@@ -4,13 +4,32 @@ module TestBase64 where
 
 import Test.QuickCheck.All
 import Test.QuickCheck.Instances ()
+import Test.QuickCheck.Property
 
+import Data.Char (ord)
+import qualified Data.Set as S
 import qualified Data.ByteString as BS
 import Codec.Binary.Base64 (encode)
 
-prop_lengthMultipleOf4 :: BS.ByteString -> Bool
-prop_lengthMultipleOf4 = \s ->
+prop_lengthMultipleOf4 s =
   BS.length (encode s) `rem` 4 == 0
+
+prop_endsWithPadding b =
+  collect r
+    $ suffix `BS.isSuffixOf` (encode b)
+ where
+  r = BS.length b `rem` 3
+  suffix = BS.replicate
+    ((3 - r) `rem` 3)
+    (fromIntegral $ ord '=')
+
+prop_outputAlphabet b =
+  collect (S.size used)
+    $ used `S.isSubsetOf` allowed
+ where
+  used = S.fromList . BS.unpack $ encode b
+  allowed = S.fromList . map (fromIntegral . ord) $
+    ['A'..'Z'] ++ ['a'..'z'] ++ ['0'..'9'] ++ ['+','/']
 
 ----------------------------------------------------------------------
 return []
